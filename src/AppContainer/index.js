a91553f3d6a361643d8feb300f111e6f5e088c50
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-09-01 00:23:22 -0300

    Implement AppContainer

---
+++ src/AppContainer/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Router } from 'react-native-router-flux'
+import scenes from '../routes/scenes'
+
+import { Provider } from 'react-redux'
+
+export default class AppContainer extends Component {
+  render() {
+    return (
+      <Provider store={this.props.store}>
+        <Router scenes={scenes} />
+      </Provider>
+    )
+  }
+}
+
+// const propTypes = {
+//   store: PropTypes.object.isRequired
+// }
